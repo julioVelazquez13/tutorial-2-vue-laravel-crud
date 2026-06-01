0788f286cea4036dcff7f42ba6607714ac057e50
Julio Velazquez <juliovel1313@gmail.com>
2026-06-01 02:35:23 -0400

    Integra Vue en Laravel y crea componentes CRUD

---
+++ resources/js/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>CRUD Laravel + Vue</h1>
+
+    <button @click="addCrud">Add</button>
+
+    <CrudComponent
+      v-for="crud in cruds"
+      :key="crud.id"
+      :id="crud.id"
+      :name="crud.title"
+      :color="crud.color"
+      @update="updateCrud"
+      @delete="deleteCrud"
+    />
+  </div>
+</template>
+
+<script>
+import CrudComponent from './components/CrudComponent.vue'
+
+export default {
+  components: {
+    CrudComponent
+  },
+
+  data() {
+    return {
+      cruds: [
+        {
+          id: 1,
+          title: 'Primer registro',
+          color: 'red'
+        }
+      ]
+    }
+  },
+
+  methods: {
+    addCrud() {
+      console.log('Add')
+    },
+
+    updateCrud(id, color) {
+      console.log('Update', id, color)
+    },
+
+    deleteCrud(id) {
+      console.log('Delete', id)
+    }
+  }
+}
+</script>
---
+++ resources/js/components/CrudComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>Name: {{ name }}</h3>
+
+    <select :value="color" @change="update">
+      <option value="red">Red</option>
+      <option value="green">Green</option>
+    </select>
+
+    <button @click="del">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id', 'name', 'color'],
+  methods: {
+    update(event) {
+      this.$emit('update', this.id, event.target.value)
+    },
+    del() {
+      this.$emit('delete', this.id)
+    }
+  }
+}
+</script>
